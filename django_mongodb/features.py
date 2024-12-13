5dc0912b431a5e2c6768694e6b533fb4280017ce
Tim Graham <timograham@gmail.com>
2024-12-12 20:49:57 -0500

    fix array index lookup

--- django_mongodb/features.py
+++ django_mongodb/features.py
@@ -89,13 +89,6 @@ class DatabaseFeatures(BaseDatabaseFeatures):
         "model_fields_.test_arrayfield.TestQuerying.test_icontains",
         # Field 'field' expected a number but got Value(1).
         "model_fields_.test_arrayfield.TestQuerying.test_exact_with_expression",
-        # int() argument must be a string, a bytes-like object or a real number, not 'list'
-        "model_fields_.test_arrayfield.TestQuerying.test_index_annotation",
-        # Wrong results
-        "model_fields_.test_arrayfield.TestQuerying.test_index",
-        "model_fields_.test_arrayfield.TestQuerying.test_index_chained",
-        "model_fields_.test_arrayfield.TestQuerying.test_index_nested",
-        "model_fields_.test_arrayfield.TestQuerying.test_order_by_slice",
         # None is $in None
         "model_fields_.test_arrayfield.TestQuerying.test_in_as_F_object",
     }
