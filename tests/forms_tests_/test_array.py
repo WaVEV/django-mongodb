845d7fd8010e1e1ad2792860674c653fce5a65d3
Emanuel Lupi <emanuel.lupi91@gmail.com>
2025-03-23 22:27:33 -0300

    Add unit tests

--- tests/forms_tests_/test_array.py
+++ tests/forms_tests_/test_array.py
@@ -134,6 +134,12 @@ class SimpleArrayFieldTests(SimpleTestCase):
         self.assertIsInstance(form_field, SimpleArrayField)
         self.assertEqual(form_field.max_length, 4)
 
+    def test_model_field_formfield_size(self):
+        model_field = ArrayField(models.CharField(max_length=27), size=4)
+        form_field = model_field.formfield()
+        self.assertIsInstance(form_field, SimpleArrayField)
+        self.assertEqual(form_field.size, 4)
+
     def test_model_field_choices(self):
         model_field = ArrayField(models.IntegerField(choices=((1, "A"), (2, "B"))))
         form_field = model_field.formfield()
