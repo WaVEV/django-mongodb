dff26acc4bb34262c4fe3acf7be736f0b049077c
Emanuel Lupi <emanuel.lupi91@gmail.com>
2025-03-26 23:22:29 -0300

    Check validator's exception message

--- tests/validators_/tests.py
+++ tests/validators_/tests.py
@@ -1,14 +1,25 @@
 from django.core.exceptions import ValidationError
 from django.test import SimpleTestCase
 
-from django_mongodb_backend.fields.validators import LengthValidator
+from django_mongodb_backend.validators import LengthValidator
 
 
 class TestValidators(SimpleTestCase):
     def test_validators(self):
         validator = LengthValidator(10)
-        with self.assertRaises(ValidationError):
+        with self.assertRaises(ValidationError) as context_manager:
             validator([])
-        with self.assertRaises(ValidationError):
+        self.assertEqual(
+            context_manager.exception.messages, ["List contains 0 items, it should contain 10."]
+        )
+        with self.assertRaises(ValidationError) as context_manager:
+            validator([1])
+        self.assertEqual(
+            context_manager.exception.messages, ["List contains 1 item, it should contain 10."]
+        )
+        with self.assertRaises(ValidationError) as context_manager:
             validator(list(range(11)))
+        self.assertEqual(
+            context_manager.exception.messages, ["List contains 11 items, it should contain 10."]
+        )
         self.assertEqual(validator(list(range(10))), None)
